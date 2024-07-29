2a35648caeeb5350216481c65576ce201b631eec
Sudhanshu <perfectsudh@gmail.com>
2024-07-29 05:51:04 +0530

    feat(react-aria): use `useDrag` to implement drag handler
    why: for some reason, setting custom drag image was not working
    
    extra-perks: handles a11y and keyboard interactions
    
    note: we may revisit this later to change the implementation

--- src/slider.tsx
+++ src/slider.tsx
@@ -1,71 +1,49 @@
 import { useRef, useState } from "react";
+import { DragPreview, useDrag } from "react-aria";
 import { cn } from "./utils";
-
-const DRAG_IMAGE_ID = "__drag-image";
 
 export function Slider() {
   const [angle, setAngle] = useState(0);
 
   const outerCircleRef = useRef<HTMLDivElement>(null);
+  const dragPreviewRef = useRef(null);
+
+  const { dragProps, dragButtonProps } = useDrag({
+    preview: dragPreviewRef,
+    onDragMove(event) {
+      const { x: clientX, y: clientY } = event;
+      // for some weird reason, the clientX and clientY for the drag event
+      // when the drag is release (i.e. dropped) is always 0
+      // which messes up with the final position of the knob
+      if (!clientX || !clientY) return;
+
+      if (outerCircleRef.current) {
+        const { top, left, width, height } =
+          outerCircleRef.current.getBoundingClientRect();
+
+        const originX = left + width / 2;
+        const originY = top + height / 2;
+
+        const x = clientX - originX;
+        const y = clientY - originY;
+
+        let angle = (Math.atan2(y, x) * 180) / Math.PI;
+
+        // this angle is offset by 90 degrees because the 0 degree is at the top
+        angle = angle + 90;
+
+        // make sure the angle is between 0 and 360 and strip any floating point
+        angle = Math.round((angle + 360) % 360);
+
+        setAngle(angle);
+      }
+    },
+    getItems() {
+      return [];
+    },
+  });
 
   const degrees = `-${angle}deg`;
-
-  const handleDrag = (event: React.DragEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-
-    const { clientX, clientY } = event;
-
-    // update the drag image
-    const dragImage = document.getElementById(DRAG_IMAGE_ID);
-    if (dragImage) {
-      // BUg: this is not working as expected
-      // TODO: fix this
-      event.dataTransfer?.setDragImage(dragImage, clientX, clientY);
-    }
-
-    // for some weird reason, the clientX and clientY for the drag event
-    // when the drag is release (i.e. dropped) is always 0
-    // which messes up with the final position of the knob
-    if (!clientX || !clientY) return;
-
-    if (outerCircleRef.current) {
-      const { top, left, width, height } =
-        outerCircleRef.current.getBoundingClientRect();
-
-      const originX = left + width / 2;
-      const originY = top + height / 2;
-
-      const x = clientX - originX;
-      const y = clientY - originY;
-
-      let angle = (Math.atan2(y, x) * 180) / Math.PI;
-
-      // this angle is offset by 90 degrees because the 0 degree is at the top
-      angle = angle + 90;
-
-      // make sure the angle is between 0 and 360 and strip any floating point
-      angle = Math.round((angle + 360) % 360);
-
-      setAngle(angle);
-    }
-  };
-
-  const handleDragStart = () => {
-    // create a drag image
-    const dragImage = document.createElement("div");
-    dragImage.style.transform = "translate(-10000px, -10000px)";
-    dragImage.style.position = "absolute";
-    dragImage.id = DRAG_IMAGE_ID;
-    document.body.appendChild(dragImage);
-  };
-
-  const handleDragEnd = () => {
-    // remove the drag image
-    const dragImage = document.getElementById(DRAG_IMAGE_ID);
-    if (dragImage) {
-      document.body.removeChild(dragImage);
-    }
-  };
 
   return (
     <div
@@ -104,18 +82,22 @@ export function Slider() {
                 "w-[calc(var(--inner-radius)*2)] h-[calc(var(--inner-radius)*2)] bg-blue-500",
                 "absolute inset-0 m-auto"
               )}
-            ></div>
+            />
             <button
+              {...dragProps}
+              {...dragButtonProps}
               className={cn(
                 "rounded-full",
                 "w-[calc(var(--ball-radius)*2)] h-[calc(var(--ball-radius)*2)] bg-red-400",
                 "absolute top-[var(--ball-x)] left-[var(--ball-y)] cursor-grab"
               )}
-              draggable={true}
-              onDrag={handleDrag}
-              onDragStart={handleDragStart}
-              onDragEnd={handleDragEnd}
-            ></button>
+            />
+            <DragPreview ref={dragPreviewRef}>
+              {() => {
+                // we don't want to show the drag preview
+                return <div className="hidden" />;
+              }}
+            </DragPreview>
           </div>
         </div>
       </div>
